fcdf07aac0796b1bf4978b4a8d745ed7a9e75155
NucaSoft <minesca7@gmail.com>
2026-02-10 19:01:45 -0400

    Practica MVC

---
+++ practica mvc/Practica MVC/Controllers/controlador.php
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practica_MVC.Controllers
+{
+    public class controlador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ practica mvc/Practica MVC/Models/modelo.php
@@ -0,0 +1,9 @@
+﻿<?php
+class Modelo {
+ public static function listar() {
+ return [
+ ["campo1" => "Producto", "campo2" => "Marca", "campo3" => "Precio", "campo2" => "Categoria", "campo3" => "Estado"]],
+ // Agrega 2 registros más aquí...
+ ];
+ }
+}
